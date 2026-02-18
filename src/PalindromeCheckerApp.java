fd0218e67c2195fdf08f987b338b67713d637fd8
karthick_2006_15 <Karthicksivanesh6@gmail.com>
2026-02-18 16:03:24 +0530

    add uc3 implementation

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,32 +1,21 @@
-/*
- * Use Case 2: Print a Hardcoded Palindrome Result
- * Palindrome Checker App
- * Version: 1.0
- */
-
-class UseCase2PalindromeCheckerApp {
+class UseCase3PalindromeCheckerApp {
 
     static void main() {
 
-        // Hardcoded string (String Literal)
-        String word = "madam";
-
-        // Variable to store reversed string
+        String original = "level";
         StringBuilder reversed = new StringBuilder();
 
-        // Logic to reverse the string
-        for (int i = word.length() - 1; i >= 0; i--) {
-            reversed.append(word.charAt(i));
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed.append(original.charAt(i));
         }
 
-        // Display original word
-        System.out.println("Given Word: " + word);
+        System.out.println("Original String : " + original);
+        System.out.println("Reversed String : " + reversed);
 
-        // Conditional check using if-else
-        if (word.contentEquals(reversed)) {
-            System.out.println("Result: The given word is a Palindrome.");
+        if (original.contentEquals(reversed)) {
+            System.out.println("Result: The given string is a Palindrome.");
         } else {
-            System.out.println("Result: The given word is NOT a Palindrome.");
+            System.out.println("Result: The given string is NOT a Palindrome.");
         }
 
         System.out.println("Program executed successfully.");
